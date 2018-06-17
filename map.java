608bb716669e2767e4c59f29e908f22b6e5cebdf
ColgateLeoAscenzi <lascenzi@colgate.edu>
2018-06-17 19:19:33 +0200

    created basic map, organizatinon of folders, character class

--- map.java
+++ map.java
@@ -9,15 +9,77 @@ import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Board extends JPanel implements ActionListener {
+public class Map extends JPanel implements ActionListener {
 
     private Timer timer;
-    private SpaceShip spaceShip;
+    private Figur figur;
     private final int DELAY = 10;
 
-    public Board() {
 
+    public Map() {
+        initMap();
         System.out.println("Created Board");
     }
+
+
+private void initMap() {
+
+    addKeyListener(new TAdapter());
+    setFocusable(true);
+    setBackground(Color.black);
+    setDoubleBuffered(true);
+
+    figur = new Figur();
+
+    timer = new Timer(DELAY, this);
+    timer.start();
+}
+
+@Override
+public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    doDrawing(g);
+
+    Toolkit.getDefaultToolkit().sync();
+}
+
+private void doDrawing(Graphics g) {
+
+    Graphics2D g2d = (Graphics2D) g;
+
+    g2d.drawImage(figur.getImage(), figur.getX(),
+        figur.getY(), this);
+}
+
+
+private class TAdapter extends KeyAdapter {
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        figur.keyReleased(e);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        figur.keyPressed(e);
+    }
+}
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        step();
+    }
+
+    private void step() {
+
+        figur.move();
+
+        repaint(figur.getX()-1, figur.getY()-1,
+                figur.getWidth()+2, figur.getHeight()+2);
+    }
+
 
 }
---
+++ resources/inhalt.md
@@ -0,0 +1,1 @@
+This folder should contain resources such as player Images or Sounds
