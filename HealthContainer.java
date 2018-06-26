27c4b5c18817862f0294bf3a3b24823e1d7e133a
ColgateLeoAscenzi <lascenzi@colgate.edu>
2018-06-26 19:45:44 +0200

    added the beginning of the health conttainer class, added elizabeth base sprites

---
+++ HealthContainer.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Image;
+import java.util.List;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.ImageIcon;
+
+
+public class HealthContainer extends JPanel{
+    public HealthContainer() {
+
+        initMap();
+
+    }
+
+    private void initMap() {
+
+
+    }
+
+}
--- RunGame.java
+++ RunGame.java
@@ -1,5 +1,6 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class RunGame extends JFrame {
 
@@ -11,13 +12,15 @@ public class RunGame extends JFrame {
     private void initUI() {
 
         add(new Map());
-
+        //add(new HealhtContainer());
         setTitle("Map");
-        setSize(800, 800);
+        setSize(800, 900);
+
 
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 
     public static void main(String[] args) {
